ce1e0cb1d4febe7c3f3d332eccee5df3d6002e1b
renanbatel <renanbatel@gmail.com>
2020-01-20 11:23:21 -0300

    create image implemented

--- src/create-image.ts
+++ src/create-image.ts
@@ -1,15 +1,21 @@
 import { APIGatewayProxyEvent } from '../lib/aws/types/aws-lambda.interface';
+import { getRepository } from '../lib/db';
 import { middy } from '../lib/middy';
 import { validateSchema } from '../lib/middy/middlewares';
 import { CreateImageDTO } from './dto';
+import { Image } from './models';
+
+export async function createImage(image: CreateImageDTO) {
+  const repository = await getRepository<Image>(Image);
+  const data = await repository.save(image);
+
+  return repository.findOne(data.id);
+}
 
 export async function createImageApiGatewayHandler(event: APIGatewayProxyEvent<CreateImageDTO>) {
-  return {
-    body: {
-      message: 'Hello Guys',
-      event,
-    },
-  };
+  const body = await createImage(event.bodyValidated);
+
+  return { body };
 }
 
 export const apiGatewayHandler = middy(createImageApiGatewayHandler).use(validateSchema(CreateImageDTO));
