40bb8038d781d39feae14b6d7884530c9747756f
renanbatel <renanbatel@gmail.com>
2020-01-21 14:02:59 -0300

    jest configured

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testURL: 'http://localhost:3000/',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  testRegex: '(/__tests__/.*(\\.|/)(integrated|unitary|e2e))\\.ts$',
+  collectCoverage: true,
+  coverageReporters: ['html', 'lcov', 'text-summary', 'text'],
+  testTimeout: 10000,
+};
